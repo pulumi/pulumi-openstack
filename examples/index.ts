818d30ec785d09579da571a94e278289bfb880d8
Fraser Waters <frassle@gmail.com>
2018-06-28 13:04:35 +0100

    Add mini example

---
+++ examples/index.ts
@@ -0,0 +1,9 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as os from "@pulumi/openstack";
+
+// flavorName and imageName will need changing based on what's availible in the
+// OpenStack installation your using.
+const instance = new os.compute.Instance("test", {
+	flavorName: "s1-2",
+	imageName: "Ubuntu 16.04"
+});
