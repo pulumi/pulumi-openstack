a990fb36e4ebfcb393c8fff87aab46b1e52074b0
Luke Hoban <luke@pulumi.com>
2018-07-08 15:13:12 -0700

    Add test coverage

---
+++ examples/examples_test.go
@@ -0,0 +1,43 @@
+// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+
+package examples
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/testing/integration"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExamples(t *testing.T) {
+	region := os.Getenv("OS_AUTH_URL")
+	if region == "" {
+		t.Skipf("Skipping test due to missing OS_AUTH_URL environment variable")
+	}
+	cwd, err := os.Getwd()
+	if !assert.NoError(t, err, "expected a valid working directory: %v", err) {
+		return
+	}
+
+	base := integration.ProgramTestOptions{
+		Tracing: "https://tracing.pulumi-engineering.com/collector/api/v1/spans",
+	}
+
+	examples := []integration.ProgramTestOptions{
+		base.With(integration.ProgramTestOptions{
+			Dir: path.Join(cwd, "webserver"),
+			Dependencies: []string{
+				"@pulumi/openstack",
+			},
+		}),
+	}
+
+	for _, ex := range examples {
+		example := ex
+		t.Run(example.Dir, func(t *testing.T) {
+			integration.ProgramTest(t, &example)
+		})
+	}
+}
