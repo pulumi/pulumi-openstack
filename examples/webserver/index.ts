d5f6dc42d63a8d4ee1c110954a670c60817f37a6
Eric Rudder <ericrudder@users.noreply.github.com>
2018-07-21 08:29:14 -0700

    fixed typo in example

--- examples/webserver/index.ts
+++ examples/webserver/index.ts
@@ -1,7 +1,7 @@
 import * as os from "@pulumi/openstack";
 
-// flavorName and imageName will need changing based on what's availible in the
-// OpenStack installation your using.
+// flavorName and imageName may need to change based on what's availible in the
+// OpenStack installation you're using.
 const instance = new os.compute.Instance("test", {
 	flavorName: "s1-2",
 	imageName: "Ubuntu 16.04",
