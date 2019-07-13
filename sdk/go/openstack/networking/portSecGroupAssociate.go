23473f508138f759bd8a5f2526b950fc7d7aca27
stack72 <public@paulstack.co.uk>
2019-07-13 03:00:35 +0300

    Regenerate SDKs based on latest pulumi-terraform

--- sdk/go/openstack/networking/portSecGroupAssociate.go
+++ sdk/go/openstack/networking/portSecGroupAssociate.go
@@ -8,6 +8,7 @@ import (
 	"github.com/pulumi/pulumi/sdk/go/pulumi"
 )
 
+// > This content is derived from https://github.com/terraform-providers/terraform-provider-openstack/blob/master/website/docs/r/networking_port_secgroup_associate_v2.html.markdown.
 type PortSecGroupAssociate struct {
 	s *pulumi.ResourceState
 }
