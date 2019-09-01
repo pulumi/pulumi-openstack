355b0de9144ba74f6b0d928762b4a7faf8c17bd4
stack72 <public@paulstack.co.uk>
2019-09-01 22:12:00 +0300

    Reenable the Webserver test

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -3,7 +3,9 @@
 package examples
 
 import (
+	"github.com/stretchr/testify/assert"
 	"os"
+	"path"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/testing/integration"
@@ -14,26 +16,25 @@ func TestExamples(t *testing.T) {
 	if region == "" {
 		t.Skipf("Skipping test due to missing OS_AUTH_URL environment variable")
 	}
-	// cwd, err := os.Getwd()
-	// if !assert.NoError(t, err, "expected a valid working directory: %v", err) {
-	// 	return
-	// }
+	cwd, err := os.Getwd()
+	if !assert.NoError(t, err, "expected a valid working directory: %v", err) {
+		return
+	}
 
-	// base := integration.ProgramTestOptions{
-	// 	Tracing: "https://tracing.pulumi-engineering.com/collector/api/v1/spans",
-	// }
+	base := integration.ProgramTestOptions{
+		Tracing: "https://tracing.pulumi-engineering.com/collector/api/v1/spans",
+	}
 
 	examples := []integration.ProgramTestOptions{
-		// TODO[pulumi/pulumi-openstack#45]: Fix an re-enable this test
-		// base.With(integration.ProgramTestOptions{
-		// 	Dir: path.Join(cwd, "webserver"),
-		// 	Dependencies: []string{
-		// 		"@pulumi/openstack",
-		// 	},
-		// 	// One change is known to occur during refresh of the resources in this example:
-		// 	// `~  openstack:compute:Instance test updated changes: + blockDevices,personalities,schedulerHints``
-		// 	ExpectRefreshChanges: true,
-		// }),
+		base.With(integration.ProgramTestOptions{
+			Dir: path.Join(cwd, "webserver"),
+			Dependencies: []string{
+				"@pulumi/openstack",
+			},
+			// One change is known to occur during refresh of the resources in this example:
+			// `~  openstack:compute:Instance test updated changes: + blockDevices,personalities,schedulerHints``
+			ExpectRefreshChanges: true,
+		}),
 	}
 
 	for _, ex := range examples {
