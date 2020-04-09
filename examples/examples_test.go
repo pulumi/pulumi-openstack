d18b5c91d3de88fddfd17237c051f444fdb90dad
stack72 <public@paulstack.co.uk>
2020-04-09 13:51:41 +0100

    Upgrade to Pulumi v2.0.0-beta.2

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -7,7 +7,7 @@ import (
 	"path"
 	"testing"
 
-	"github.com/pulumi/pulumi/pkg/testing/integration"
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
 
 func TestAccWebserver(t *testing.T) {
