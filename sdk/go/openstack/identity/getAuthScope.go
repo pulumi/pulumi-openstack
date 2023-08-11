2a10b952ab0b46dc8b062ca279ec4d94a9d55a74
Alex Qiu <aqiu@pulumi.com>
2023-08-11 14:18:53 -0700

    Bump package-publisher action version (#346)

--- sdk/go/openstack/identity/getAuthScope.go
+++ sdk/go/openstack/identity/getAuthScope.go
@@ -93,8 +93,9 @@ import (
 //
 // import (
 //
-// "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-// "github.com/pulumi/pulumi-http/sdk/go/http"
+//	"github.com/pulumi/pulumi-http/sdk/go/http"
+//	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+//
 // )
 // func main() {
 // pulumi.Run(func(ctx *pulumi.Context) error {
